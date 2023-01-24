d648a65a88cc65d9cfef639846c26fcc21914dc6
KOZHIN SALM <121977138+ilsDe1@users.noreply.github.com>
2023-01-24 14:20:21 +0100

    Update Yaxi.js

--- Yaxi.js
+++ Yaxi.js
@@ -14,15 +14,15 @@ Yaxi.on("ready", () => {
   setInterval(function() {
     let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
     Yaxi.user.setActivity(STREAMING, {      
-    type: "STREAMING", 
+    type: "As Up Dev", 
     url: "https://www.twitch.tv/faith"
     });
   }, 2000);
 });
 ///////////////////////
 Yaxi.on("ready", () => {
-    var join = Yaxi.channels.get(""); /// id voice
+    var join = Yaxi.channels.get("858796966129827870"); /// id voice
     if (join) join.join();
   });
 //////////////// tokn lera da bne 
-Yaxi.login("");
+Yaxi.login("OTIyMDc5NjE3Nzc3NTMyOTQ4.GwLXI7.BUwQHXpFUpOcbr7aQouoiul9FeQmKCa8m72hhI");
